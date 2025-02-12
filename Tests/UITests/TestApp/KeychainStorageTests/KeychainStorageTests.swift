c7a6a8ef3c329eff7258fe556e4c25451724f2a5
Lukas Kollmer <hey@lukaskollmer.de>
2025-02-12 10:47:11 +0100

    undo accidentally commiting my bundle id

--- Tests/UITests/TestApp/KeychainStorageTests/KeychainStorageTests.swift
+++ Tests/UITests/TestApp/KeychainStorageTests/KeychainStorageTests.swift
@@ -290,11 +290,11 @@ final class KeychainStorageTests: TestAppTestCase {
             .secureEnclave(requireUserPresence: false),
             .secureEnclave(requireUserPresence: true),
             .keychain(requireUserPresence: false, accessGroup: nil),
-            .keychain(requireUserPresence: false, accessGroup: "A485NLSB8K.de.lukaskollmer.jvnriwnifowe"),
+            .keychain(requireUserPresence: false, accessGroup: "637867499T.edu.stanford.spezi.storage.testapp"),
             .keychain(requireUserPresence: true, accessGroup: nil),
-            .keychain(requireUserPresence: true, accessGroup: "A485NLSB8K.de.lukaskollmer.jvnriwnifowe"),
+            .keychain(requireUserPresence: true, accessGroup: "637867499T.edu.stanford.spezi.storage.testapp"),
             .keychainSynchronizable(accessGroup: nil),
-            .keychainSynchronizable(accessGroup: "A485NLSB8K.de.lukaskollmer.jvnriwnifowe")
+            .keychainSynchronizable(accessGroup: "637867499T.edu.stanford.spezi.storage.testapp")
         ]
         #if targetEnvironment(simulator)
         storageOptionsToTest.remove(at: 1) // secure enclave with user presence isn't supported on the simulator
